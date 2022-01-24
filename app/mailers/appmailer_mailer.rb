82ed6ae7c6b64ff33c49ac9653ca123ae93ad836
Anthony Bonasso <bonassoa@gmail.com>
2022-01-24 14:49:28 -0500

    email testing

--- app/mailers/appmailer_mailer.rb
+++ app/mailers/appmailer_mailer.rb
@@ -6,7 +6,7 @@ class AppmailerMailer < ApplicationMailer
         late_users = Array.new
 
         @books.histories.each do |history|
-            if(Date.today == history.chkDate + 7 && history.returnedOn == nil)
+            if(history.returnedOn == nil)
                 late_users << history.email
             end
 
